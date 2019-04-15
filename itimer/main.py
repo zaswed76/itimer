2af30f28445a2dbde18ef42a3aa95141dfbd2fdd
Admin <zaswed76@gmail.com>
2019-04-15 09:39:57 +0300

    add start btn

--- itimer/main.py
+++ itimer/main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtCore import QSize
+
 from itimer.config import Config
 from itimer.gui import basewindow, configwindow, widgets
 
@@ -9,11 +11,14 @@ from itimer.gui import basewindow, configwindow, widgets
 STYLE_PATH = "style/style.qss"
 CFG_PATH = "etc/cfg.json"
 CFG_ICON_PATH = "resource/icons/cfg.png"
+START_ICON_PATH = "resource/icons/start.png"
 
 
 class Widget(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.resize(500, 200)
+
         self.cfg = Config()
         self.cfg.load(CFG_PATH)
         self.data = self.cfg.data
@@ -30,12 +35,27 @@ class Widget(QtWidgets.QMainWindow):
         self.stack.setCurrentIndex(0)
 
         self.cfg_show_btn = widgets.Btn(self.base_widget,
-                                   "show_cfg_btn", CFG_ICON_PATH)
+                                        "show_cfg_btn", CFG_ICON_PATH)
         self.cfg_show_btn.clicked.connect(self.show_settings)
 
+        self.start_timer_btn = widgets.Btn(self.base_widget,
+                                           "start_timer_btn", START_ICON_PATH)
+        self.start_timer_btn.clicked.connect(self.start_timer)
+        self.start_timer_btn.setIconSize(QSize(64, 64))
+        self.start_timer_btn.setFixedSize(64, 64)
+        self.start_timer_btn.move(
+            self.rect().right() - self.start_timer_btn.rect().width(),
+            self.rect().top())
+
         self.cfg_close_btn = widgets.Btn(self.cfg_widget,
-                                   "close_cfg_btn", CFG_ICON_PATH)
+                                         "close_cfg_btn", CFG_ICON_PATH)
         self.cfg_close_btn.clicked.connect(self.close_settings)
+
+        print(self.rect().right())
+        print(self.start_timer_btn.rect().width())
+
+    def start_timer(self):
+        print("start")
 
     def show_settings(self):
         print(1)
