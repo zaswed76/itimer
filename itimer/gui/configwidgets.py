3493c8328ece62ae25989aff0b3cceb934f9c7f6
Admin <zaswed76@gmail.com>
2019-04-20 16:49:59 +0300

    add start btn

--- itimer/gui/configwidgets.py
+++ itimer/gui/configwidgets.py
@@ -1,6 +1,10 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
+
+class OptionNameLabel(QtWidgets.QLabel):
+    def __init__(self, *__args):
+        super().__init__(*__args)
 
 
 class SignalSelectionWidget(QtWidgets.QFrame):
@@ -11,8 +15,8 @@ class SignalSelectionWidget(QtWidgets.QFrame):
         self.btn1 = QtWidgets.QPushButton("1")
         self.btn2 = QtWidgets.QPushButton("2")
         self.btn3 = QtWidgets.QPushButton("+")
-        self.user_signal_lb = QtWidgets.QLabel("3")
-        self.lb = QtWidgets.QLabel("сигнал")
+        self.user_signal_lb = QtWidgets.QLabel("metallica.mp3")
+        self.lb = OptionNameLabel("сигнал")
         self.hbox.addWidget(self.lb)
         self.hbox.addWidget(self.btn1)
         self.hbox.addWidget(self.btn2)
@@ -24,8 +28,60 @@ class IntervalSelectionWidget(QtWidgets.QFrame):
         super().__init__()
         self.setStyleSheet("background-color: white")
         self.hbox = QtWidgets.QHBoxLayout(self)
-        self.lb = QtWidgets.QLabel("сигнал")
+        self.lb = OptionNameLabel("интервал")
+        self.interval = QtWidgets.QTimeEdit()
+        self.interval.setDisplayFormat("HH:mm:ss")
+
         self.hbox.addWidget(self.lb)
+        self.hbox.addWidget(self.interval)
+
+class DurationSignalWidget(QtWidgets.QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("background-color: white")
+        self.hbox = QtWidgets.QHBoxLayout(self)
+        self.lb = OptionNameLabel("длительность")
+        self.duration = QtWidgets.QSpinBox()
+
+
+        self.hbox.addWidget(self.lb)
+        self.hbox.addWidget(self.duration)
+
+class AutostartWidget(QtWidgets.QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("background-color: white")
+        self.hbox = QtWidgets.QHBoxLayout(self)
+        self.lb = OptionNameLabel("автостарт")
+        self.autostar = QtWidgets.QCheckBox()
+
+
+        self.hbox.addWidget(self.lb)
+        self.hbox.addWidget(self.autostar)
+
+class SelectingCyclingWidget(QtWidgets.QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("background-color: white")
+        self.hbox = QtWidgets.QHBoxLayout(self)
+        self.lb = OptionNameLabel("зациклить")
+        self.cycling = QtWidgets.QCheckBox()
+
+
+        self.hbox.addWidget(self.lb)
+        self.hbox.addWidget(self.cycling)
+
+class VolumeWidget(QtWidgets.QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("background-color: white")
+        self.hbox = QtWidgets.QHBoxLayout(self)
+        self.lb = OptionNameLabel("громкость")
+        self.volume = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+
+
+        self.hbox.addWidget(self.lb)
+        self.hbox.addWidget(self.volume)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
